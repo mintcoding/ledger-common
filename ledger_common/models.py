138d4bb3bd0e52f6505178c3d77d2daa1d02ab28
Brendan Blackford <brendan.blackford@dbca.wa.gov.au>
2021-02-14 11:54:21 +0800

    Fix FK refs

--- ledger_common/models.py
+++ ledger_common/models.py
@@ -21,7 +21,7 @@ class AbstractRegion(models.Model):
 
 
 class AbstractDistrict(models.Model):
-    region = models.ForeignKey(AbstractRegion, related_name='districts')
+    region = models.ForeignKey('ledger_common.AbstractRegion', related_name='districts')
     name = models.CharField(max_length=200, unique=True)
     code = models.CharField(max_length=3)
     archive_date = models.DateField(null=True, blank=True)
@@ -270,7 +270,7 @@ class AbstractProposal(models.Model):
     review_status = models.CharField('Review Status', max_length=30, choices=REVIEW_STATUS_CHOICES,
                                      default=REVIEW_STATUS_CHOICES[0][0])
 
-    approval = models.ForeignKey('disturbance.Approval',null=True,blank=True)
+    approval = models.ForeignKey('ledger_common.Approval',null=True,blank=True)
 
     previous_application = models.ForeignKey('self', on_delete=models.PROTECT, blank=True, null=True)
     proposed_decline_status = models.BooleanField(default=False)
